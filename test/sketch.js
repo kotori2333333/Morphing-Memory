8038e7dbc67485d32bc5125eb29585b60e0a9b32
kotori2333333 <2072955170@qq.com>
2022-03-31 17:15:33 +0800

    Add files via upload
    ver2

--- test/sketch.js
+++ test/sketch.js
@@ -2,17 +2,20 @@ let dcgan;
 let museum;
 let pen;
 let pc;
+let base;
+
+let inst = true;
 
 let vector = [];
 let grids = [];
 
 
-let gridChange = 0;
+let gridChange = 0.05;
 
 let scroll;
 let range = 0;
 
-let penSize = 0
+let penSize = 1.5
 
 function preload() {
     dcgan = ml5.DCGAN('model/manifest.json');
@@ -20,6 +23,7 @@ function preload() {
     museum = loadImage('assets/museum.png')
     pen = loadImage('assets/pen.png')
     pc = loadImage('assets/pc.png')
+    base = loadImage('assets/base.png')
 }
 
 function setup() {
@@ -49,21 +53,24 @@ function keyPressed(){
   for (let i = 0; i < 512; i++) {
       vector[i] = random(-1, 1);
   }}
+  else if(key == 't'){
+    inst = !inst;
+  }
 }
 
 function walk() {
     if(keyIsPressed){
       if(key == "w"){
-        gridChange += 0.005;
+        gridChange += 0.008;
       }
       else if(key == "s"){
-        gridChange -= 0.005;
+        gridChange -= 0.008;
       }
       else if(key == "a"){
-        penSize -= 0.01;
+        penSize -= 0.015;
       }
       else if(key == "d"){
-        penSize += 0.01;
+        penSize += 0.015;
       }
 
       gridChange = constrain(gridChange,-0.1,0.1)
@@ -110,17 +117,40 @@ function displayImage(err, result) {
     image(pen,mouseX,mouseY-60,170*1.5*penScale,120*1.5*penScale)
 
 
-    textSize(100)
-    // text(gridChange,1000,500)
 
-
-    fill(map(gridChange,-0.1,0.1,0,255))
+    image(base,750+900,1030-40,2388/5,1668/5)
+    fill(map(gridChange,-0.1,0.1,0,255),180)
     noStroke()
-    inkSize = map(penSize,0,4,20,100);
-    // ellipse(1000,700,inkSize,inkSize)
+    inkSize = map(penSize,0,4,50,150);
+    ellipse(730+900,1060-40,inkSize,inkSize)
 
     pop()
 
+    push()
+
+    fill(255,150,150)
+    textAlign(CENTER)
+    if(inst){
+      textSize(25)
+// W to make ink brighter
+// S to to make ink darker
+// A to decrease brush size
+// D to increase brush size
+// R to resize
+
+      text("Draw on the computer screen and see how the artwork responds",700,950);
+      text("W to make ink brighter",700,1000);
+      text("S to to make ink darker",700,1050);
+      text("A to decrease brush size",700,1100);
+      text("D to increase brush size",700,1150);
+      text("R to resize",700,1200);
+
+    }
+    else{
+      textSize(70)
+      text("Press t to show instructions",700,1100)
+    }
+    pop()
 
     generate();
 }
